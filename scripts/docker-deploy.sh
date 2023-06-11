511fb61fd125c28fe2bca2a68684fed6ef5d8f7a
Sakshi <74068861+sakshi-cse@users.noreply.github.com>
2023-06-12 02:49:19 +0530

    Update docker-deploy.sh

--- scripts/docker-deploy.sh
+++ scripts/docker-deploy.sh
@@ -4,11 +4,11 @@ cd /app
 
 
 # build the latest Docker image
-docker build -t node-app:latest .
+sudo docker build -t node-app:latest .
 
 # Stop and remove the existing container (if running)
-docker stop my-container || true
-docker rm my-container || true
+sudo docker stop my-container || true
+sudo docker rm my-container || true
 
 # Run the Docker container with the updated image
-docker run -d --name my-container -p 80:80 node-app:latest
+sudo docker run -d --name my-container -p 80:80 node-app:latest
