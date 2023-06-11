a2bbd0afd51803982cb3284e1e3c27e8809aeaeb
Sakshi <74068861+sakshi-cse@users.noreply.github.com>
2023-06-12 02:14:47 +0530

    Update docker-deploy.sh

--- scripts/docker-deploy.sh
+++ scripts/docker-deploy.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+cd /var/www/html
+
+
+# build the latest Docker image
+docker build -t node-app:latest .
+
 # Stop and remove the existing container (if running)
 docker stop my-container || true
 docker rm my-container || true
-
-# Pull the latest Docker image
-docker build node-app:latest .
 
 # Run the Docker container with the updated image
 docker run -d --name my-container -p 80:80 node-app:latest
